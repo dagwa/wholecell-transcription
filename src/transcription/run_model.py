706dee8bcf3dbf48c3f1b3cba82a6bc8779cacd5
0u812 <0u812@github.com>
2015-03-13 11:37:47 +0100

    add comments to run model script

--- src/transcription/run_model.py
+++ src/transcription/run_model.py
@@ -1,16 +1,20 @@
 from roadrunner import RoadRunner, Logger
 
-#Logger.setLevel(Logger.LOG_DEBUG)
+#Logger.setLevel(Logger.LOG_DEBUG) # too verbose
 Logger.setLevel(Logger.LOG_WARNING)
 
-# Simulate model
+# Load model
+
 rr = RoadRunner('/tmp/tx.sbml')
 #rr = RoadRunner('/tmp/tx-reduced.sbml')
 #rr = RoadRunner('/tmp/antsbml.xml')
+
+# Simulate model
 results = rr.simulate(0, 10, 30, integrator='gillespie')
 
 print(results)
 
+# Write results to temp file
 with open('/tmp/sim_res.csv', 'w') as f:
     f.write(str(results))
 
